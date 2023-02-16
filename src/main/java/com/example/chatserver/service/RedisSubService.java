e7e9cea7e58e3096ae96eea2d7aea7db7721838f
janu01ary <janu01ary@gmail.com>
2023-02-16 23:53:31 +0900

    feat: Redis 구독 및 구독한 메시지 받아오는 기능 추가

--- src/main/java/com/example/chatserver/service/RedisSubService.java
+++ src/main/java/com/example/chatserver/service/RedisSubService.java
@@ -1,7 +1,6 @@
 package com.example.chatserver.service;
 
 import com.example.chatserver.dto.MessageDTO;
-import com.example.chatserver.dto.MessageType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
@@ -11,20 +10,13 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class RedisSubService implements MessageListener {
-    private final WebSocketPubService webSocketPubService;
     private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
         try {
             MessageDTO messageDTO = mapper.readValue(new String(message.getBody()), MessageDTO.class);
-            System.out.println("redis에서 받음 : " + message);
-            if (messageDTO.getType() == MessageType.SEND) {
-                webSocketPubService.sendWebSocketMessage("/topic/" + messageDTO.getRoomId(), messageDTO);
-            }
-            else if (messageDTO.getType() == MessageType.TYPING || messageDTO.getType() == MessageType.TYPED) {
-                webSocketPubService.sendWebSocketMessage("/topic/typing/" + messageDTO.getRoomId(), messageDTO);
-            }
+            System.out.println("redis에서 받음 : " + messageDTO.getType() + ", " + messageDTO.getSenderId());
         } catch (Exception e) {
             e.printStackTrace();
         }
