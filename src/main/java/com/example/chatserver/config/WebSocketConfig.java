89cae71aef9faa0da97e80d571d4494bdc8642dd
janu01ary <janu01ary@gmail.com>
2023-02-16 11:25:28 +0900

    feat: WebSocket message 보내기

--- src/main/java/com/example/chatserver/config/WebSocketConfig.java
+++ src/main/java/com/example/chatserver/config/WebSocketConfig.java
@@ -11,8 +11,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config){
-        config.enableSimpleBroker("/topic");
+    public void configureMessageBroker(MessageBrokerRegistry registry){
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
